e4cb870ca7603c9b66daf970cd66ee2b836d7d8f
EricCel <ericcortijotoyo@gmail.com>
2026-08-12 17:11:54 -0400

    Add files via upload

---
+++ 65-numpy-between-interval/main.py
@@ -0,0 +1,10 @@
+#Use np.linspace() to generate 50 evenly spaced points between the interval 0 and 1.
+
+#-Importing : The module!
+import numpy as np
+
+#-The spaced points : Evenly!
+the_spaced_array = np.linspace(0,1) #By default, linspace uses an interval of 50
+
+#-Displaying : The array!
+print(the_spaced_array)
